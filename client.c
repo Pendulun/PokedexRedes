8511d5f9291870fb46f25f5b6719bc9ae5ea6da9
Pendulun <danielcampos14@hotmail.com.br>
2021-11-15 11:59:03 -0300

    Refatorando inicio cliente - Cliente

--- client.c
+++ client.c
@@ -5,11 +5,17 @@
 #include <string.h>
 
 #include <unistd.h>
-
 #include <sys/types.h>
 #include <sys/socket.h>
-
 #include <arpa/inet.h>
+
+struct Client{
+    struct sockaddr_storage cstorage;
+    struct sockaddr *caddr;
+    socklen_t caddrlen;
+    char caddrstr[BUFSZ];
+    int socket;
+};
 
 void usage(int argc, char **argv) {
 	printf("usage: %s <server IP> <server port>\n", argv[0]);
@@ -17,37 +23,52 @@ void usage(int argc, char **argv) {
 	exit(EXIT_FAILURE);
 }
 
+void inicia_client(struct Client* client, int argc, char **argv){
+	//o sockaddr_storage é um sockaddr muito grande que, eventualmente, vira um in ou in6
+	struct sockaddr_storage storage;
+	//Faz o parse do que passado como argumento para ipv4 ou ipv6 se possível
+	if (0 != addrparse(argv[1], argv[2], &(client->cstorage))) {
+		usage(argc, argv);
+	}
+
+	//Criando um socket depois de realizar o parse do endereço passado como argumento
+	client->socket = socket(client->cstorage.ss_family, SOCK_STREAM, 0);
+
+	//verifica o retorno e sai caso deu erro
+	if (client->socket == -1) {
+		logexit("socket");
+	}
+
+	//Converte o storage para a struct mais abstrata sockaddr
+	client->caddr = (struct sockaddr *)(&client->cstorage);
+
+	if (0 != connect(client->socket, client->caddr, sizeof(client->cstorage))) {
+		logexit("connect");
+	}
+
+	addrtostr(client->caddr, client->caddrstr, BUFSZ);
+
+	printf("connected to %s\n", client->caddrstr);
+
+}
+
 int main(int argc, char **argv) {
 	if (argc < 3) {
 		usage(argc, argv);
 	}
 
-	//o sockaddr_storage é um sockaddr muito grande que, eventualmente, vira um in ou in6
-	struct sockaddr_storage storage;
-	//Faz o parse do que passado como argumento para ipv4 ou ipv6 se possível
-	if (0 != addrparse(argv[1], argv[2], &storage)) {
-		usage(argc, argv);
-	}
+	struct Client cliente;
+	inicia_client(&cliente, argc, argv);
 
-	//Criando um socket depois de realizar o parse do endereço passado como argumento
-	int my_socket;
-	my_socket = socket(storage.ss_family, SOCK_STREAM, 0);
-
-	//verifica o retorno e sai caso deu erro
-	if (my_socket == -1) {
-		logexit("socket");
-	}
-
-	//Converte o storage para a struct mais abstrata sockaddr
-	struct sockaddr *addr = (struct sockaddr *)(&storage);
-	if (0 != connect(my_socket, addr, sizeof(storage))) {
+	/*
+	if (0 != connect(cliente.socket, cliente.caddr, sizeof(cliente.cstorage))) {
 		logexit("connect");
 	}
 
 	char addrstr[BUFSZ];
-	addrtostr(addr, addrstr, BUFSZ);
+	addrtostr(cliente.caddr, cliente.caddrstr, BUFSZ);
 
-	printf("connected to %s\n", addrstr);
+	printf("connected to %s\n", cliente.caddrstr);*/
 
 	//Inicializa uma string de tamanho BUFSZ que irá conter a mensagem a ser enviada
 	char buf[TAM_MAX_MSG];
@@ -77,7 +98,7 @@ int main(int argc, char **argv) {
 
 		//Envia a mensagem passando o socket "s" e retorna quantos bytes foram enviados de fato
 		//printf("Aqui 1 CL\n");
-		size_t bytes_recebidos_pacote = send(my_socket, buf, strlen(buf)+1, 0);
+		size_t bytes_recebidos_pacote = send(cliente.socket, buf, strlen(buf)+1, 0);
 		//Se enviou um número de bytes diferente do próprio tamanho da mensagem, deu erro
 		if (bytes_recebidos_pacote != strlen(buf)+1) {
 			logexit("send");
@@ -92,9 +113,9 @@ int main(int argc, char **argv) {
 			printf("Esperando resposta\n");
 			//O recv pode enviar a mesma mensagem em partes
 			//Dessa forma, temos que ficar recebendo até que ele envie 0 bytes
-			bytes_recebidos_pacote = recv(my_socket, buf + total_bytes_recebido, TAM_MAX_MSG - total_bytes_recebido, 0);
+			bytes_recebidos_pacote = recv(cliente.socket, buf + total_bytes_recebido, TAM_MAX_MSG - total_bytes_recebido, 0);
 			short int jaLeu = 0;
-            for(int i=total_bytes_recebido;i<(TAM_MAX_MSG-total_bytes_recebido);i++){
+            for(int i=total_bytes_recebido;i<(total_bytes_recebido+bytes_recebidos_pacote);i++){
                 char barra_n[2] = "\n";
                 char fim_string[2] = "\0";
 			    /*if(strcmp(&buf[i],fim_string)==0){
@@ -125,6 +146,6 @@ int main(int argc, char **argv) {
 		}
 	}
 	
-	close(my_socket);
+	close(cliente.socket);
 	exit(EXIT_SUCCESS);
 }
